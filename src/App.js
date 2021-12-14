4f455b994dda83fef271e0c07465bef5ba010cfc
Desiree Burrell <desireeburrell333@desirees-air.myfiosgateway.com>
2021-12-14 09:16:44 -0500

    added the four pages with nothing in them

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        My name is Desiree Burrell and I'm going to master React!
-        </p> 
-        <a
-          className="App-link"
-          href="https://github.com/DesireeBurrell"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Check Me Out!
-        </a>
-      </header>
+     
     </div>
   );
 }
